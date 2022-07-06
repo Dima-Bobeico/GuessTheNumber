0c5905f85488c328e61511c7d6c02c8ae1a7e69e
Viorel Bobeico <88560048+Jiorel@users.noreply.github.com>
2022-07-06 15:41:52 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [number, setNumber] = React.useState(1);
+
+  const getRandomNumber = () => {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    setNumber(randomNumber);
+  };
+
   return (
     <View style={styles.container}>
+      <Text>{number}</Text>
       <Text>Write a number between 0-10 </Text>
       <TextInput style={styles.TextInputStyle} placeholder={"Number:"} />
-      <Button title="check" style={styles.ButtonStyle} />
+      <Button
+        title="check"
+        style={styles.ButtonStyle}
+        onPress={() => getRandomNumber()}
+      />
     </View>
   );
 }
