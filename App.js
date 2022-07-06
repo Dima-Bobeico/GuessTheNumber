17f3a1a51e3551373abacc44666baa820d83ab7f
Viorel Bobeico <88560048+Jiorel@users.noreply.github.com>
2022-07-06 15:06:46 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Type:</Text>
+      <TextInput style={styles.TextInputStyle} placeholder={"Number:"} />
+      <Button title="check" style={styles.ButtonStyle} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  TextInputStyle: {
+    backgroundColor: "#F0EBE3",
+    borderColor: "#E4DCCF",
+    borderWidth: 3,
+    borderRadius: 10,
+  },
+  ButtonStyle: {
+    color: "blue",
+  },
+});
